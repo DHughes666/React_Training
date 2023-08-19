64559fdb68c564b53291b122a1e951037a02fda7
DHughes666 <graziemolto6@gmail.com>
2023-08-20 00:36:33 +0100

    Detail refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Card from './Card.jsx';
 import contacts from '../contacts.js';
+import Avatar from "./Avatar.jsx";
 
 function App() {
     return <div>
+    <h1 className="heading">My Contacts</h1>
+    <Avatar img="https://pbs.twimg.com/profile_images/1497392721803284480/_VHZjROl_400x400.jpg" />
     <Card 
         name= {contacts[0].name}
         img= {contacts[0].imgURL}
