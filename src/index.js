2d4089465d6eee74d5027a5f18b85abafa6dd16d
DHughes666 <graziemolto6@gmail.com>
2023-08-20 15:48:49 +0100

    Arrow Exercises completed

--- src/index.js
+++ src/index.js
@@ -6,29 +6,28 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
+console.log(numbers);
 
 ////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+const mapNumbers = numbers.map(x => x * 2);
+console.log(mapNumbers);
 
 //////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+const filterNumbers = numbers.filter(x => x < 10);
+console.log(filterNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+var reduceNumber = numbers.reduce((
+  accumulator, currentNumber) => accumulator + currentNumber);
+console.log(reduceNumber);
 
 ////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+const findNumber = numbers.find(num => num > 10);
+console.log(findNumber);
 
 ////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+const findIndexNumber = numbers.findIndex(num => num > 10);
+console.log(findIndexNumber);
+
+
