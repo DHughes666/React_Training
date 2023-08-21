d0539eed6c00d1218880720d4f8337b03ab75c55
DHughes666 <graziemolto6@gmail.com>
2023-08-21 07:34:17 +0100

    Conditional Rendering initiated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,16 @@
 import React from "react";
-import Card from './Card.jsx';
-import contacts from '../contacts.js';
-import Avatar from "./Avatar.jsx";
-
-function createCard(contact) {
-    return (
-    <Card 
-        id = {contact.id}
-        key = {contact.id}
-        name= {contact.name} 
-        img = {contact.imgURL}
-        tel = {contact.tel}
-        email = {contact.email}
-    />
-    );
-}
 
 function App() {
-    return <div>
-    <h1 className="heading">My Contacts</h1>
-    
-    {contacts.map(createCard)}
-
-    {/* <Card 
-        name= {contacts[0].name}
-        img= {contacts[0].imgURL}
-        tel= {contacts[0].tel}
-        email= {contacts[0].email}
-    />
-
-    <Card 
-        name= {contacts[1].name}
-        img= {contacts[1].imgURL}
-        tel= {contacts[1].tel}
-        email= {contacts[1].email}
-    />
-
-    <Card 
-        name= {contacts[2].name}
-        img= {contacts[2].imgURL}
-        tel= {contacts[2].tel}
-        email= {contacts[2].email}
-    /> */}
-
-   
-
-   
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
     </div>
+  );
 }
 
 export default App;
