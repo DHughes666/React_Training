9100a9148a97c55a2294032859832d0df47f0113
DHughes666 <graziemolto6@gmail.com>
2023-08-21 22:22:31 +0100

    Changing Complex State

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function App() {
-
-  const [name, setName] = useState("");
-  const [headingText, setHeadingText] = useState("");
-
-  function handleChange(event){
-    setName(event.target.value);
-  }
-
-  function handleClick(event){
-    setHeadingText(name);
-    event.preventDefault();
-  }
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
 
   return (
     <div className="container">
-      <h1>Hello {headingText} </h1>
-      <input 
-      onChange={handleChange} 
-      type="text" placeholder="What's your name?"
-      value={name}
-      />
-      <button onClick={handleClick}>Submit</button>
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <input name="email" placeholder="Email" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
-
-// In HTML, form elements such as <input>, <textarea>, and <select> 
-// typically maintain their own state and update it based on user input. 
-// In React, mutable state is typically kept in the state property of 
-// components, and only updated with setState().
 
 export default App;
