904157d912da0f7fa7946a5ce8c453023c3588b1
DHughes666 <graziemolto6@gmail.com>
2023-08-21 20:15:28 +0100

    Destructuring - Smart Refactoring by Angela

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,37 @@
 import React from "react";
 import cars from "../practice";
-import animals, {useAnimals} from "../data";
-
 
 function App() {
 
-const [cat, dog] = animals;
+  const [honda, tesla] = cars
 
-console.log(useAnimals(dog));
-const [animal, makesound] = useAnimals(cat);
-console.log(animal);
-makesound();
+  const {speedStats: {topSpeed: hondaTopSpeed}} = honda
+  const {speedStats: {topSpeed: teslaTopSpeed}} = tesla
 
-console.log("--------------------------------");
-
-const [honda, tesla] = cars
-
-// const {model, coloursByPopularity, speedStats} = honda 
-// const {topSpeed: hondaTopSpeed, zeroToSixty} = speedStats
-
-const {models, coloursByPopularity, 
-  speedStats: {topSpeed: hondaTopSpeed}} = honda
-
-  const [hondaTopColour, hondaColour] = coloursByPopularity
+  const {coloursByPopularity: [hondaTopColour]} = honda
+  const {coloursByPopularity: [teslaTopColour]} = tesla
 
 
-console.log(honda);
-console.log(hondaTopColour);
-console.log(hondaTopSpeed);
+  return (
+    <table>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+    </table>
+  )
 
-console.log("---------------------------------------");
 
-const {model: modelT, coloursByPopularity: Tcol, speedStats: Tsped} = tesla
-const {topSpeed: teslaTopSpeed, zeroToSixty: teslaZeroToSixty} = Tsped
-const [teslaTopColour, teslaColour] = Tcol
-
-console.log(hondaTopSpeed);
-console.log(tesla);
-
-  // return (
-  //   <table>
-  //     <tr>
-  //       <th>Brand</th>
-  //       <th>Top Speed</th>
-  //     </tr>
-  //     <tr>
-  //       <td>{tesla.model}</td>
-  //       <td>{teslaTopSpeed}</td>
-  //       <td>{teslaTopColour}</td>
-  //     </tr>
-  //     <tr>
-  //       <td>{honda.model}</td>
-  //       <td>{hondaTopSpeed}</td>
-  //       <td>{hondaTopColour}</td>
-  //     </tr>
-  //   </table>
-  // )
-
-  return <h1>Hi</h1>
-
- 
 }
 
 export default App;
