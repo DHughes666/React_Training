4977fec6403d4b4e465c848eae94b37ca3dec02d
DHughes666 <graziemolto6@gmail.com>
2023-08-21 21:10:07 +0100

    Event handling -- my way

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+  const [headingText, setHeadingText] = useState("Hello")
+  let [colori, setColori] = useState("white")
+  
+  function handleClick(){
+    setHeadingText("Submitted")
+  }
+
+  function mouseOn(){
+    setColori("black")
+  }
+
+  function mouseOut(){
+    setColori("white")
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button style={{backgroundColor: colori }}
+      onClick={handleClick} >Submit</button>
     </div>
   );
 }
