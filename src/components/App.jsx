7f4173215a85ed69e1a4cd7e2851b84d25cfdf50
DHughes666 <graziemolto6@gmail.com>
2023-08-21 22:03:52 +0100

    React Forms - Angela Way

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,35 @@
 import React, {useState} from "react";
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
+
+  function handleChange(event){
+    setName(event.target.value);
+  }
+
+  function handleClick(event){
+    setHeadingText(name);
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText} </h1>
+      <input 
+      onChange={handleChange} 
+      type="text" placeholder="What's your name?"
+      value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
+
+// In HTML, form elements such as <input>, <textarea>, and <select> 
+// typically maintain their own state and update it based on user input. 
+// In React, mutable state is typically kept in the state property of 
+// components, and only updated with setState().
 
 export default App;
