b2ceab7cf9021f5b8a6e7a3a2c22068c3a543ec9
DHughes666 <graziemolto6@gmail.com>
2023-08-23 18:54:05 +0100

    Spread Operator Initiated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,24 +11,9 @@ function App() {
     const {value, name} = event.target
 
     setContact((preValue) => {
-      if (name === 'fName') {
-        return {
-          fName: value,
-          lName: preValue.lName,
-          email: preValue.email
-        }
-      } else if (name === 'lName') {
-        return {
-          fName: preValue.fName,
-          lName: value,
-          email: preValue.email
-        }
-      } else {
-        return {
-          fName: preValue.fName,
-          lName: preValue.lName,
-          email: value
-        }
+      return {
+        ...preValue,
+        name: value
       }
     })
   }
